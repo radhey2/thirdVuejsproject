81218e409b53366d15134e8dff4c2a3ec30ad737
lenovo <radheshyamb9@gmail.com>
2022-06-07 22:51:09 +0530

    ADDED : Is a conditional that will display information depending on meeting a requirement. This can be anything- buttons, forms, divs, components

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ <div id="app">
+  <h3>What is your favorite kind of taco?</h3>
+  <textarea v-model="tacos"></textarea>
+  <br>
+  <button v-show="tacos" type="submit">Let us know!</button>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      tacos: ''
+    }
+  }
+}
+</script>
